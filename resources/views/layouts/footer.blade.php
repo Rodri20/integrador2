2ea8cd5dc3a6080b4cd66932943f9cb873f7acf8
Hiad <88218366+hiadPalomino@users.noreply.github.com>
2024-07-06 00:22:12 -0500

    Hiad Actualizacion footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -7,18 +7,18 @@
     <div class="container py-5">
         <div class="row g-5">
             <div class="col-lg-3 col-md-6">
-                <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Company</h4>
-                <a class="btn btn-link" href="">About Us</a>
-                <a class="btn btn-link" href="">Contact Us</a>
-                <a class="btn btn-link" href="">Reservation</a>
-                <a class="btn btn-link" href="">Privacy Policy</a>
-                <a class="btn btn-link" href="">Terms & Condition</a>
+                <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Mr. Burguer</h4>
+                <a class="btn btn-link" href="">Sobre Nosotros</a>
+                <a class="btn btn-link" href="">Contactanos</a>
+                <a class="btn btn-link" href="">Reservaciones</a>
+                <a class="btn btn-link" href="">Politica de privacidad</a>
+                <a class="btn btn-link" href="">Terminos y condiciones</a>
             </div>
             <div class="col-lg-3 col-md-6">
-                <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Contact</h4>
-                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
+                <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Información de contacto</h4>
+                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Jesús María 123, Lima, Peru</p>
                 <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
+                <p class="mb-2"><i class="fa fa-envelope me-3"></i>mister@burguer.com</p>
                 <div class="d-flex pt-2">
                     <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
                     <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
@@ -27,18 +27,18 @@
                 </div>
             </div>
             <div class="col-lg-3 col-md-6">
-                <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Opening</h4>
-                <h5 class="text-light fw-normal">Monday - Saturday</h5>
+                <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Horario de atención</h4>
+                <h5 class="text-light fw-normal">Lunes - Sabados</h5>
                 <p>09AM - 09PM</p>
-                <h5 class="text-light fw-normal">Sunday</h5>
+                <h5 class="text-light fw-normal">Domingos</h5>
                 <p>10AM - 08PM</p>
             </div>
             <div class="col-lg-3 col-md-6">
-                <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Newsletter</h4>
-                <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+                <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Unete a Mr.Burguer</h4>
+                <p>Suscríbete para estar al tanto de nuestros sorteos mensuales, promociones especiales y eventos</p>
                 <div class="position-relative mx-auto" style="max-width: 400px;">
-                    <input class="form-control border-primary w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-                    <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
+                    <input class="form-control border-primary w-100 py-3 ps-4 pe-5" type="text" placeholder="Email">
+                    <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">Suscribirse</button>
                 </div>
             </div>
         </div>
@@ -47,17 +47,17 @@
         <div class="copyright">
             <div class="row">
                 <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    &copy; <a class="border-bottom" href="#">Your Site Name</a>, All Right Reserved. 
+                    &copy; <a class="border-bottom" href="#">Mr.Burguer</a>, Todos los derechos reservados. 
                     
                     <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                    Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a><br><br>
-                    Distributed By <a class="border-bottom" href="https://themewagon.com" target="_blank">ThemeWagon</a>
+                    Diseñado por <a class="border-bottom" href="https://htmlcodex.com">Grupo 1</a><br><br>
+                    Distribuido por <a class="border-bottom" href="https://themewagon.com" target="_blank">ThemeWagon</a>
                 </div>
                 <div class="col-md-6 text-center text-md-end">
                     <div class="footer-menu">
-                        <a href="">Home</a>
+                        <a href="">Inicio</a>
                         <a href="">Cookies</a>
-                        <a href="">Help</a>
+                        <a href="">Ayuda</a>
                         <a href="">FQAs</a>
                     </div>
                 </div>
