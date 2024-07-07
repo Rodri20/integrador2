1700976a9575d2733eec12544a2daad90c726859
Sergio <101843666+cessio@users.noreply.github.com>
2024-07-07 02:52:14 -0500

    2 Actualizacion - Sergio Alvarez

--- resources/views/pages/servicios.blade.php
+++ resources/views/pages/servicios.blade.php
@@ -6,8 +6,8 @@
         <div class="container-xxl py-5">
             <div class="container">
                 <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                    <h5 class="section-title ff-secondary text-center text-primary fw-normal">Our Services</h5>
-                    <h1 class="mb-5">Explore Our Services</h1>
+                    <h5 class="section-title ff-secondary text-center text-primary fw-normal">Nuestros servicios</h5>
+                    <h1 class="mb-5">Explora nuestros servicios</h1>
                 </div>
                 <div class="row g-4">
                     <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
