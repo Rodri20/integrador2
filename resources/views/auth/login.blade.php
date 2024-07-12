65cf2103579f00a091475d3016335f7079656ce6
Sergio <101843666+cessio@users.noreply.github.com>
2024-07-12 18:19:26 -0500

    4 actualizacion - Sergio Alvarez

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
 </head>
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8" style="width: 56%;">
             <div class="card">
                 <div class="row g-0">
                     <!-- Columna de la imagen -->
@@ -69,7 +69,7 @@
 
                                     <div class="row mb-0">
                                         <div class="col-md-8 offset-md-4">
-                                            <button type="submit" class="btn btn-primary">
+                                            <button type="submit" class="btn btn-primary" style="width: 50%">
                                                 {{ __('Login') }}
                                             </button>
 
