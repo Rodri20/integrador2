43e382d074e973bc7392a7954421eacc1dd5a66c
Hiad <88218366+hiadPalomino@users.noreply.github.com>
2024-07-14 19:39:32 -0500

    Mejorar la estética del formulario de login y register
    - Centrar y estilizar los elementos del formulario
    - Mejorar la apariencia del botón
    - Aplicar clases de Bootstrap para una mejor disposición

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<head>
+    <link href="{{ asset('css/estilos.css') }}" rel="stylesheet">
+</head>
+<div class="container mt-5">
     <div class="row justify-content-center">
-        <div class="col-md-8" style="width: 45%;">
-            <div class="card" style="margin: 10px;"  >
+        <div class="col-md-8">
+            <div class="card" style="margin: 10px;">
                 <center><div class="card-header">{{ __('Register') }}</div></center>
 
                 <div class="card-body">
@@ -12,9 +15,7 @@
                         @csrf
 
                         <div class="row mb-3">
-                            {{-- <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label> --}}
-
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 offset-md-3">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="Name" required autocomplete="name" autofocus>
 
                                 @error('name')
@@ -26,10 +27,8 @@
                         </div>
 
                         <div class="row mb-3">
-                            {{-- <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label> --}}
-
-                            <div class="col-md-6 offset-md-4">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}"  placeholder="Email Address" required autocomplete="email">
+                            <div class="col-md-6 offset-md-3">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Email Address" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -40,9 +39,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            {{-- <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label> --}}
-
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 offset-md-3">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" required autocomplete="new-password">
 
                                 @error('password')
@@ -54,15 +51,13 @@
                         </div>
 
                         <div class="row mb-3">
-                            {{-- <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label> --}}
-
-                            <div class="col-md-6 offset-md-4">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Confirm Password"  required autocomplete="new-password">
+                            <div class="col-md-6 offset-md-3">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Confirm Password" required autocomplete="new-password">
                             </div>
                         </div>
 
                         <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 offset-md-3">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="terms" id="terms" required>
                                     <label class="form-check-label" for="terms">
@@ -73,15 +68,15 @@
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="botn btn btn-primary" style="width: 100%;">
+                            <div class="col-md-6 offset-md-3">
+                                <button type="submit" class="btn btn-primary" style="width: 100%;">
                                     {{ __('Register') }}
                                 </button>
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                        <div class="row mb-3 mt-3">
+                            <div class="col-md-6 offset-md-3 text-center">
                                 <p><a href="{{ url('/login') }}">{{ __('¿Ya tengo Cuenta?') }}</a></p>
                             </div>
                         </div>
